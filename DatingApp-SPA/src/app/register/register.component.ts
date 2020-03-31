480aadddc62658f0d33c0274347e95f52c25a3cf
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-03-31 16:41:39 -0400

    adding more field to the form plus date format

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
 import { ThrowStmt } from '@angular/compiler';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker/public_api';
 
 @Component({
   selector: 'app-register',
@@ -12,21 +14,61 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
 
   model: any = {};
+  registerForm: FormGroup;
+  colorTheme = 'theme-dark-blue';
+  bsConfig: Partial<BsDatepickerConfig>;
 
-  constructor(private authSevice: AuthService, private alertify: AlertifyService) { }
+  constructor(private authSevice: AuthService, private alertify: AlertifyService, private fb: FormBuilder) { }
 
 
 
   ngOnInit() {
+    // different way of implementing the form.
+    // this.registerForm = new FormGroup({
+    //   username: new FormControl('', Validators.required),
+    //   password: new FormControl('', [Validators.required, Validators.minLength(4),
+    //     Validators.maxLength(8)]),
+    //   confirmPassword: new FormControl('', Validators.required)
+    // }, this.passwordMatchValidator);
+
+    // better way using angular/forms:
+    this.createRegisterFrom();
+
+    this.bsConfig = {
+      containerClass: 'theme-dark-blue',
+    };
+  }
+
+
+  createRegisterFrom() {
+    this.registerForm = this.fb.group({
+      gender: ['female', ],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required],
+    }, {
+      validators: this.passwordMatchValidator
+    });
+  }
+
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {mismatch: true};
   }
 
   register() {
-    this.authSevice.register(this.model)
-    .subscribe(() => {
-      this.alertify.success('registration successful');
-    }, error => {
-      this.alertify.error(error);
-    });
+    // this.authSevice.register(this.model)
+    // .subscribe(() => {
+    //   this.alertify.success('registration successful');
+    // }, error => {
+    //   this.alertify.error(error);
+    // });
+
+    console.log(this.registerForm.value);
   }
 
   cancel() {
