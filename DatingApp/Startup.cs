792946b17442ac3cd1587e83dde9142ddec0f522
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-04-03 15:58:07 -0400

    action filters api

--- DatingApp/Startup.cs
+++ DatingApp/Startup.cs
@@ -62,6 +62,7 @@ namespace DatingApp
 
                 };
             });
+            services.AddScoped<LogUserActivity>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
