b70151b4979e8f1f2bd6a115872ac623195db85c
Romain Penaruiz <rom.penaruiz@gmail.com>
2020-04-14 10:30:37 -0400

    filtering back end done

--- DatingApp/Helpers/UserParams.cs
+++ DatingApp/Helpers/UserParams.cs
@@ -11,6 +11,13 @@ namespace DatingApp.Helpers
             get { return pageSize;}
             set { pageSize = (value > PageSize) ? MaxPageSize : value; }
         }
+
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+
+        public int MinAge { get; set; } = 18;
+
+        public int MaxAge { get; set; } = 99;
         
     }
 }
